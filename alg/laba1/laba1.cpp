629dad0f8bfc12ece4d7f1bb85a3f0a25e4ea563
tearfu1 <axuior12@gmail.com>
2023-09-10 11:33:48 +0200

    laba1 added

---
+++ alg/laba1/laba1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::vector<char> brackets(0);
+	std::cout << "Введите строку" << std::endl;
+	std::string str;
+	std::cin >> str;
+	
+	bool flag = true;
+	for (int i = 0; i < str.size(); ++i)
+	{
+		if (str[i] == '(' || str[i] == '[' || str[i] == '{')
+		{
+			brackets.push_back(str[i]);
+		}
+		else if (str[i] == ')' || str[i] == ']' || str[i] == '}')
+		{
+			if (brackets.size())
+			{
+				if (str[i] == ')')
+				{
+					if (brackets[brackets.size() - 1] != '(' )
+					{
+						flag = false;
+						break;
+					}
+					else
+					{
+						brackets.pop_back();
+					}
+				}
+				else 
+				{
+					if (brackets[brackets.size() - 1] != str[i] - 2)
+					{
+						flag = false;
+						break;
+					}
+					else
+					{
+						brackets.pop_back();
+					}
+				}
+			}
+			else
+			{
+				flag = false;
+				break;
+			}
+		}
+	}
+
+	if (brackets.size() > 0)
+	{
+		flag = false;
+	}
+
+	if (flag)
+	{
+		std::cout << "Строка существует";
+	}
+	else
+	{
+		std::cout << "Строка не существует";
+	}
+	return 0;
+}
