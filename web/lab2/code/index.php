03009643138bbc6643400003b7f30ff47123046c
tearfu1 <axuior12@gmail.com>
2024-03-01 10:08:05 +0200

    created laba2 and task 1 done

---
+++ web/lab2/code/index.php
@@ -0,0 +1,11 @@
+<?php
+//task 1
+echo "task 1";
+
+$very_bad_unclear_name = "15 chicken wings";
+$order = &$very_bad_unclear_name;
+$order .= " and cola";
+echo "\nYour order is: $very_bad_unclear_name.";
+echo "\n";
+
+
