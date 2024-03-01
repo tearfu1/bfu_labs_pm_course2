ebc12a28532692c009c1290f692540a27ad858ff
tearfu1 <axuior12@gmail.com>
2024-03-01 10:09:41 +0200

    task 2 done

--- web/lab2/code/index.php
+++ web/lab2/code/index.php
@@ -8,4 +8,22 @@ $order .= " and cola";
 echo "\nYour order is: $very_bad_unclear_name.";
 echo "\n";
 
+//task 2
+echo "task 2\n";
+
+$newInt = 52;
+echo $newInt;
+echo "\n";
+
+$newFloat = 3.14;
+echo $newFloat;
+echo "\n";
+
+echo 15 - 3;
+echo "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month;
+echo "\n";
 
