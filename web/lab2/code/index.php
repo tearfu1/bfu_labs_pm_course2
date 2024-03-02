9360413be9e2458a9d3c7ea14c49cd93432c69bd
tearfu1 <axuior12@gmail.com>
2024-03-02 23:04:06 +0200

    tasks 18, 19 and beginning of 20 done

--- web/lab2/code/index.php
+++ web/lab2/code/index.php
@@ -59,26 +59,32 @@ echo "\n";
 
 //task 14
 echo "\ntask 14\n";
+
+#
 $a = 10;
 $b = 3;
 $c = $a % $b;
 echo "a % b is $c";
 echo "\n";
 
+#
 if ($c == 0) {
     echo "a is divisible by b without remainder\n";
 } else {
     echo "a is divisible by b with remainder: $c\n";
 }
 
+#
 $st = pow(2, 10);
 echo "2 to the 10th power is $st";
 echo "\n";
 
+#
 $root1 = sqrt(245);
 echo "square root of 245 is $root1";
 echo "\n";
 
+#
 $array1 = [4, 2, 5, 19, 13, 0, 10];
 $sum = 0;
 foreach ($array1 as $item) {
@@ -88,6 +94,7 @@ $root2 = sqrt($sum);
 echo "square root from squared elemts sum is $root2";
 echo "\n";
 
+#
 $root3 = sqrt(379);
 $root3Num = round($root3);
 $root3Ten = round($root3, 1);
@@ -99,6 +106,7 @@ echo "\n";
 echo "to thousands $root3Thousand";
 echo "\n";
 
+#
 $root4 = sqrt(587);
 $root4Top = ceil($root4);
 $root4Bot = floor($root4);
@@ -106,6 +114,7 @@ $root4Bot = floor($root4);
 $array2 = ["ceil" => $root4Top, "floor" => $root4Bot];
 var_dump($array2);
 
+#
 $array3 = [4, -2, 5, 19, -130, 0, 10];
 $minim = min($array3);
 echo "min from array3 is $minim";
@@ -115,10 +124,12 @@ $maxim = max($array3);
 echo "max from array3 is $maxim";
 echo "\n";
 
+#
 $randNum = rand(1, 100);
 echo "random num is $randNum";
 echo "\n";
 
+#
 $array4 = [];
 
 while (sizeof($array4) < 11) array_push($array4, rand(1, 100));
@@ -128,12 +139,14 @@ foreach ($array4 as $item) {
 }
 echo "\n";
 
+#
 $a = rand(1, 100);
 $b = rand(1, 100);
 $abs = abs($a - $b);
 echo "a: $a, b: $b module is $abs";
 echo "\n";
 
+#
 $array5 = [1, 2, -1, -2, 3, -3];
 $array6 = [];
 foreach ($array5 as $item) {
@@ -150,6 +163,7 @@ foreach ($array6 as $item) {
 }
 echo "\n";
 
+#
 $num = rand(1, 100);
 echo "new num is $num";
 echo "\n";
@@ -172,6 +186,7 @@ foreach ($array7 as $item) {
 }
 echo "\n";
 
+#
 $array8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 $cnt = 0;
 $temp_sum = 0;
@@ -185,6 +200,8 @@ echo "\n";
 
 //task 15
 echo "\ntask 15\n";
+
+#
 function printStringReturnNumber(): int
 {
     $num = rand(1, 100);
@@ -193,13 +210,17 @@ function printStringReturnNumber(): int
     return $num;
 }
 
+#
 $myNum = printStringReturnNumber();
 
+#
 echo "myNum is $myNum";
 echo "\n";
 
 //task 16
 echo "\ntask 16\n";
+
+#
 function increaseEnthusiasm(string $str)
 {
     return $str . "!";
@@ -207,6 +228,7 @@ function increaseEnthusiasm(string $str)
 
 echo increaseEnthusiasm("im fine") . "\n";
 
+#
 function repeatThreeTimes(string $str)
 {
     return $str . $str . $str;
@@ -214,8 +236,10 @@ function repeatThreeTimes(string $str)
 
 echo repeatThreeTimes("is it echo?") . "\n";
 
+#
 echo increaseEnthusiasm(repeatThreeTimes("its absolutely echo")) . "\n";
 
+#
 function cut(string $str, int $num = 10): string
 {
     return substr($str, 0, $num);
@@ -224,6 +248,7 @@ function cut(string $str, int $num = 10): string
 $newStr = "hello im here to talk about something";
 echo "cut function: " . cut($newStr, 20) . "\n";
 
+#
 function arrayOutput(array $arr)
 {
     if (sizeof($arr) == 0) {
@@ -240,6 +265,7 @@ arrayOutput([1, 2, 3]);
 
 $newNum = rand(100, 1000);
 
+#
 function numSumDecreaser(int $num)
 {
     $sum = 0;
@@ -259,6 +285,7 @@ echo "\n";
 //task 17
 echo "\ntask 17\n";
 
+#1
 $array9 = [];
 
 for ($i = 1; $i < 10; ++$i) {
@@ -274,6 +301,7 @@ foreach ($array9 as $item) {
 }
 echo "\n";
 
+#2
 function arrayFill(string $elem, int $num): array
 {
     $newArray = [];
@@ -290,6 +318,7 @@ foreach ($array10 as $item) {
 }
 echo "\n";
 
+#3
 $array11 = [[1, 2, 3], [4, 5], [6]];
 echo "2-D array is ";
 foreach ($array11 as $item) {
@@ -306,6 +335,7 @@ foreach ($array11 as $item1) {
 }
 echo "Sum of 2-dimensional array is : " . $newSum . "\n";
 
+#4
 echo "2-D array creation: ";
 $array12 = [];
 $firstDim = 3;
@@ -326,16 +356,20 @@ foreach ($array12 as $item) {
 }
 echo "\n";
 
+#5
 $array13 = [2, 5, 3, 9];
 $result = $array13[0] * $array13[1] + $array13[2] * $array13[3];
 echo "result is $result" . "\n";
 
+#6
 $user = ['name' => "Maxim", 'surname' => "Vayda", 'patronymic' => "Valentinovich"];
 echo "surname is: " . $user['surname'] . ", name is: " . $user['name'] . ", patronymic is: " . $user['patronymic'] . "\n";
 
+#7
 $date = ['year' => "2024", 'month' => "march", 'day' => "1"];
 echo $date['year'] . "-" . $date['month'] . "-" . $date['day'] . "\n";
 
+#8
 $arr = ['a', 'b', 'c', 'd', 'e'];
 $size = sizeof($arr);
 echo "size of arr: $size" . "\n";
@@ -343,3 +377,78 @@ $lastElemIndex = $size - 1;
 $predLastElemIndex = $size - 2;
 echo "last elem: $arr[$lastElemIndex]" . "\n";
 echo "pred last elem: $arr[$predLastElemIndex]" . "\n";
+
+//task 18
+echo "\ntask18\n";
+
+#
+function greaterThanTen(int $a, int $b): bool
+{
+    return ($a + $b) > 10;
+}
+
+echo "greaterThanTen function result: ";
+if (greaterThanTen(10, 10)) {
+    echo "true" . "\n";
+} else {
+    echo "false" . "\n";
+}
+
+#
+function isEqual(float $a, float $b): bool
+{
+    return $a == $b;
+}
+
+echo "isEqual function result: ";
+if (isEqual(10, 10)) {
+    echo "true" . "\n";
+} else {
+    echo "false" . "\n";
+}
+
+#
+$test = 0;
+if (!$test) echo "верно" . "\n";
+
+#
+$age = rand(1, 200);
+echo "age is $age" . "\n";
+if ($age < 10 || $age > 99) {
+    echo "age is less than ten, or greater than 99: $age" . "\n";
+} else {
+    echo "age is between 10 and 99" . "\n";
+    $sum = 0;
+    while ($age > 0) {
+        $sum += $age % 10;
+        $age = floor($age / 10);
+    }
+    if ($sum <= 9) {
+        echo "sum of digits of this age is unambiguous: $sum" . "\n";
+    } else {
+        echo "the sum of the digits of this number is two digits: $sum" . "\n";
+    }
+}
+
+#
+$arr = [1, 2, 3];
+if (sizeof($arr) == 3) {
+    $sum = 0;
+    foreach ($arr as $value) $sum += $value;
+    echo "size of array is 3 and sum of its numbers is: $sum" . "\n";
+}
+
+//task 19
+echo "\ntask19 \n";
+for ($i = 1; $i <= 20; ++$i) {
+    for ($j = 0; $j < $i; ++$j) {
+        echo "x";
+    }
+    echo "\n";
+}
+
+//task 20
+echo "\ntask20 \n";
+$array14 = [1, 10, 15, 11, 63, 16, 45, 35];
+$mean = array_sum($array14) / sizeof($array14);
+echo "mean of array is $mean" . "\n";
