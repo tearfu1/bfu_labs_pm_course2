ff7a2c7d6e81bd0592d379f1b019e1f26f044d05
tearfu1 <axuior12@gmail.com>
2024-05-02 20:09:05 +0200

    labs 10-13 accepted and lab 14 fixed

--- alg/sem_4/lab14/main.py
+++ alg/sem_4/lab14/main.py
@@ -1,24 +1,21 @@
-def rabinKarp(s, w):
-    answer = []
-    n = len(s)
-    m = len(w)
+import functools
 
-    # Вычисление начальных хешей для s и w
-    hashS = hash(s[:m])
-    hashW = hash(w)
+def rabin_karp(text, st):
+    if len(st) > len(text):
+        return -1 
+    BASE = 33
+    text_hash = functools.reduce(lambda h, c: h * BASE + ord(c), text[:len(st)], 0)
+    st_hash = functools.reduce(lambda h, c: h * BASE + ord(c), st, 0)
+    power_st = BASE**max(len(st) - 1, 0) 
+    for i in range(len(st), len(text)):
+        if text_hash == st_hash and text[i - len(st):i] == st:
+            return i - len(st) 
+        text_hash -= ord(text[i - len(st)]) * power_st
+        text_hash = text_hash * BASE + ord(text[i])
+    if text_hash == st_hash and text[-len(st):] == st:
+        return len(text) - len(st)
+    return -1 
 
-    # Проход по строке s и сравнение хешей
-    for i in range(n - m + 1):
-        if hashS == hashW and s[i:i + m] == w:  # Дополнительно проверяем подстроку
-            # Если хеши совпали и подстроки совпадают, добавляем индекс i в ответ
-            answer.append(i)
-        if i < n - m:
-            # Пересчет хеша для следующего окна
-            hashS = hash(s[i + 1:i + m + 1])
-
-    return answer
-
-# Пример использования
-s = "GCATCGCAGAGAGTATACAGTACG"
-w = "GCAGAGAG"
-print(rabinKarp(s, w))
+text = "ABASABAGHSDJLKBVILASJ"
+st = "BAS"
+print(rabin_karp(text, st))
